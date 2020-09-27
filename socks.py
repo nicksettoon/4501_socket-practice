87d995d0ded25a93f676e3315714aa739c1982bc
nick <48932118+nicksettoon@users.noreply.github.com>
2020-09-27 07:41:04 -0500

    blah

--- socks.py
+++ socks.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     args = sys.argv
     
     while True:
-        mode = input("Which mode do you want?\n1. UDP\n2. TCP\nPlease input 1 or 2:\t")
+        mode = input("Which mode do you want?\n1. UDP\n2. TCP\nPlease input 1 or 2: ")
 
         if mode == "1":
             if args[1] == "-s":
